5633c451cf87987c6d1ec9d43a0c889c12589b4f
vnikonov63 <nikonov.va@yandex.ru>
2020-10-15 22:17:37 +0400

    add: -the vector of vectors to hold the game field as well as the function to print the field in the console

--- main.cpp
+++ main.cpp
@@ -34,11 +34,37 @@ void Space(int number)
   cout << string(number, '\n') << endl;
 }
 
+void DisplayTheGameField(vector<vector<string> > gameField)
+{
+  for (int i = 0; i < gameField.size(); ++i)
+  {
+    cout << "|";
+    for (int j = 0; j < gameField[i].size(); ++j)
+    {
+      cout << gameField[i][j] + "|";
+    }
+    cout << "\n";
+  }
+  cout << "---------------";
+  cout << "\n";
+}
+
 int main()
 {
   string redName;
   string yellowName;
 
+  // initialize the game field
+  vector<vector<string> > mainField;
+  for (int i = 0; i < 6; ++i)
+  {
+    vector<string> tempStep;
+    for (int j = 0; j < 7; ++j)
+    {
+      tempStep.push_back(" ");
+    }
+    mainField.push_back(tempStep);
+  }
   WrapMessage("To start the game press Y, to exit the game at any time print Q");
   string response;
   getline(cin, response);
@@ -54,4 +80,6 @@ int main()
   Space(1);
   cout << YELLOW << "Yellow player, enter the name:" << RESET << endl;
   cin >> yellowName;
+  Space(2);
+  DisplayTheGameField(mainField);
 }
